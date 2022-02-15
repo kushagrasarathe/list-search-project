937b06bb2147a8c6788f25089b51f6d3cb0ad0f0
Kushagra <kushagraaa.01@gmail.com>
2022-02-15 10:01:16 +0530

    ver1

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// variables needed to build this
+
+// getting user input
+const user_input = document.getElementById("search");
+
+// adding keyup eventlistner to user_input
+user_input.addEventListener("keyup", search);
+
+// declaring a search function 
+function search() {
+
+    const userInputValue = user_input.value;
+    const menu_list = document.getElementsByTagName("li");
+
+    for (let i = 0; i < menu_list.length; i++) {
+
+        if (menu_list[i].innerHTML.toLowerCase().includes(userInputValue)) {
+            menu_list[i].style.display = "";
+            
+        } else {
+            menu_list[i].style.display = "none";
+            alert(`${menu_list[i]}`)
+        }
+    }
+};
